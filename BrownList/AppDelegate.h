00e1c2cb2e7141578307b4bfdcc8860cd4f0b3f8
ANUJ DESHMUKH <anuj.deshmukh@student.csulb.edu>
2015-11-17 01:34:07 -0800

    Initial Commit

---
+++ BrownList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrownList
+//
+//  Created by ANUJ DESHMUKH on 11/17/15.
+//  Copyright © 2015 DESHMUKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
